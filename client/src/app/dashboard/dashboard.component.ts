a92bae988f63bbdef1c6bb551d558b930bcbc477
Davide-Furlani <davide.furlani0998@gmail.com>
2023-05-31 16:00:21 +0200

    modal

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit, ViewChild, HostListener} from '@angular/core';
 import {Apollo, gql} from "apollo-angular";
 import {DataTableDirective} from "angular-datatables";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {FilmDetailsComponent} from "../film-details/film-details.component";
 
 @Component({
 	selector: 'app-dashboard',
@@ -15,7 +17,7 @@ export class DashboardComponent implements OnInit{
 	categories: String[] = [];
 	disableFiltering = true;
 
-	constructor(private apollo: Apollo) {
+	constructor(private apollo: Apollo, private modalService: NgbModal) {
 	}
 
 	ngOnInit() {
@@ -54,17 +56,21 @@ export class DashboardComponent implements OnInit{
 				})
 			},
 			columns: [
-				{title: 'Title', data: 'title'},
+				{title: 'Title',
+				 data: 'title',
+				 render: (data, type, row) => {
+					return `<a href="#" data-film_id="${row.film_id}">${data}</a>`
+				 }
+				},
 				{title: 'Year', data: 'release_year', searchable: false},
 				{title: 'Rating', data: 'rating', searchable: false},
 				{title: 'Categories', data: 'categories', searchable: false},
 				{title: 'Language', data: 'language', searchable: false},
 				{title: 'Rental Cost', data: 'rental_rate', searchable: false},
-				{	title: 'Rent',
-					data: 'film_id',
-					render: (data) => {
+				{title: 'Rent',
+				 data: 'film_id',
+				 render: (data) => {
 					return `<button type="button" class="btn btn-outline-primary" data-film_id="${data}">RENT</button>`;}
-
 				},
 			],
 			responsive: true,
@@ -72,10 +78,12 @@ export class DashboardComponent implements OnInit{
 	}
 	@HostListener('click', ['$event'])
 	openFilm(event: Event) {
-		let eventButton = event.target as HTMLButtonElement;
+		let eventButton = event.target as HTMLElement;
 		let film_id = eventButton.getAttribute('data-film_id');
 		if (film_id) {
-			console.log(film_id)
+			const modalRef = this.modalService.open(FilmDetailsComponent);
+			modalRef.componentInstance.film_id = Number(film_id);
+			event.preventDefault();
 		}
 	}
 
